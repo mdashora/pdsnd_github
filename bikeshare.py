1799a4ef0bde9a47733068421af58dc8b47e9f65
Manish Dashora <manishdashora@gmail.com>
2020-04-21 20:34:49 -0400

    Improved docstring by adding Args

--- bikeshare.py
+++ bikeshare.py
@@ -180,7 +180,10 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users.
+    Args:
+        df - Pandas DataFrame containing city data  
+    """
 
     print("\nCalculating User Stats...\n")
     start_time = time.time()
